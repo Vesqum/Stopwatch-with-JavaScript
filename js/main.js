5ae6dc87871118ecd0a70570ecfc923be32e01a9
Vesqum <marcin.pelszyk30@gmail.com>
2022-01-27 16:48:55 +0100

    added color panel(html/css/js)

--- js/main.js
+++ js/main.js
@@ -8,16 +8,23 @@ const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
 
 const infoBtn = document.querySelector('.info')
-const changeColor = document.querySelector('.change-color')
-const modal = document.querySelector('.modal-shadow')
+const modalShadow = document.querySelector('.modal-shadow')
 const modalCloseBtn = document.querySelector('.close')
+
+const changeColor = document.querySelector('.change-color')
+const colors = document.querySelector('.colors')
+const colorOne = document.querySelector('.one')
+const colorTwo = document.querySelector('.two')
+const colorThree = document.querySelector('.three')
+const colorFour = document.querySelector('.four')
 
 let countTime;
 let minutes = 0;
 let secounds = 0;
-
-
 let timeArr = [];
+
+let root = document.documentElement;
+
 
 const handleStart = () => {
 
@@ -89,10 +96,45 @@ const showHistory = () => {
     })
 }
 
+const showModal = () => {
+    
+    if(!(modalShadow.style.display === 'block')) {
+        modalShadow.style.display = 'block'
+    } else {
+        modalShadow.style.display = 'none'
+    };
+
+    modalShadow.classList.toggle('modal-animation')
+}
+
+
+const showColors = () => {
+    colors.classList.toggle('show-colors')
+}
+
 
 startBtn.addEventListener('click', handleStart);
 pauseBtn.addEventListener('click', handlePause);
 stopBtn.addEventListener('click', handleStop);
 restBtn.addEventListener('click', handleReset);
 historyBtn.addEventListener('click', showHistory);
+infoBtn.addEventListener('click', showModal);
+modalCloseBtn.addEventListener('click', showModal);
+window.addEventListener('click', e => e.target === modalShadow ? showModal() : false);
+changeColor.addEventListener('click', showColors)
 
+colorOne.addEventListener('click',() => {
+    root.style.setProperty('--first-color', '#FA1406')
+})
+
+colorTwo.addEventListener('click',() => {
+    root.style.setProperty('--first-color', '#9edb2c')
+})
+
+colorThree.addEventListener('click',() => {
+    root.style.setProperty('--first-color', '#1b96dd')
+})
+
+colorFour.addEventListener('click',() => {
+    root.style.setProperty('--first-color', '#ebd831')
+})
